e9aaeb6ffdcd3dc958d8edb7fec4b0e0e1a51763
djr812 <stillspirit@gmail.com>
2025-02-25 11:00:29 +1000

    Import requirements

---
+++ main.py
@@ -0,0 +1,13 @@
+import requests
+from datetime import datetime, timedelta
+from flask import Flask, render_template, request, redirect, url_for
+from dotenv import load_dotenv
+import os
+import json
+import yfinance as yf
+
+
+load_dotenv()
+
+app = Flask(__name__, template_folder="templates", static_folder="static")
+application = app
